a2abca359fafe8a02eeb0390d603998dce82d3a4
Kévin <kevin.blaise24@gmail.com>
2024-03-08 15:06:52 +0100

    add projects controller and validators

--- app/controllers/projects_controller.ts
+++ app/controllers/projects_controller.ts
@@ -1,22 +1,42 @@
 import type { HttpContext } from '@adonisjs/core/http';
+import { createValidator, updateValidator } from '#validators/project';
 import Project from '#models/project';
 
 export default class ProjectsController {
-    public async index({}: HttpContext) {
+    public async index({ response }: HttpContext) {
         const projects = await Project.all();
 
-        return projects;
+        return response.ok(projects);
     }
 
-    public async show({ params }: HttpContext) {
+    public async show({ params, response }: HttpContext) {
         const project = await Project.findOrFail(params.id);
 
-        return project;
+        return response.ok(project);
     }
 
-    public async create({}: HttpContext) {}
+    async create({ request, response }: HttpContext) {
+        const payload = await createValidator.validate(request.all());
+        const project = await Project.create(payload);
 
-    public async update({ params }: HttpContext) {}
+        return response.created(project);
+    }
 
-    public async delete({ params }: HttpContext) {}
+    public async update({ request, params, response }: HttpContext) {
+        const data = { ...request.all(), id: params.id }
+        const project = await Project.findOrFail(data.id);
+        
+        const payload = await updateValidator.validate(data);
+        project.merge(payload);
+        await project.save();
+
+        return response.ok(project);
+    }
+
+    public async delete({ params, response }: HttpContext) {
+        const project = await Project.findOrFail(params.id);
+        await project.delete();
+
+        return response.noContent();
+    }
 }
---
+++ app/validators/project.ts
@@ -0,0 +1,49 @@
+import vine from '@vinejs/vine';
+
+export const createValidator = vine.compile(
+    vine.object({
+        name: vine
+            .string()
+            .minLength(3)
+            .maxLength(254)
+            .unique(async (db, value) => {
+                const project = await db.from('projects').where('name', value).first();
+                return !project;
+            }),
+        users: vine
+            .array(
+                vine.number().exists(async (db, value) => {
+                    const user = await db.from('users').where('id', value).first();
+                    return !!user;
+                })
+            )
+            .distinct()
+            .nullable(),
+    })
+);
+
+export const updateValidator = vine.compile(
+    vine.object({
+        name: vine
+            .string()
+            .minLength(3)
+            .maxLength(254)
+            .unique(async (db, value, field) => {
+                const project = await db
+                    .from('projects')
+                    .where('name', value)
+                    .whereNot('id', field.data.id)
+                    .first();
+                return !project;
+            }),
+        users: vine
+            .array(
+                vine.number().exists(async (db, value) => {
+                    const user = await db.from('users').where('id', value).first();
+                    return !!user;
+                })
+            )
+            .distinct()
+            .nullable(),
+    })
+);
